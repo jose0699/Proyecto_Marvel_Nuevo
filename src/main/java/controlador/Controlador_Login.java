af6191f2840f961160be125ce2cdb14774d5a91e
jose0699 <joseluislopezasencio704@gmail.com>
2023-06-24 23:07:03 -0400

    Ultima version con Maven

---
+++ src/main/java/controlador/Controlador_Login.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto_marvel_bd.Proyecto_Marvel_BD;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import Vistas.Login;
+
+/**
+ *
+ * @author José Luis López
+ */
+public class Controlador_Login implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
